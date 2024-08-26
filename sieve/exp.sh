ad9f9813e25d9bcee50b06701a39749eec3ed314
Matvei Karibdzhanov <matveichrk2@gamil.com>
2024-08-26 21:10:30 +0900

    Track

--- sieve/exp.sh
+++ sieve/exp.sh
@@ -13,7 +13,7 @@ for ((i=0; i<30; i++)) do
     for ((j=0; j<10; j++)) do
         if [ `./check_statistics.sh  $1 $i $j` -ne "0" ] 
         then 
-            echo "data_${1}_${2}_${3}" >> started.txt
+            echo "data_${1}_${i}_${j}" >> started.txt
             bsub -ql ./data.sh ${1} ${i} ${j}
         fi
     done
