2d673e0d2d07ae92ec51ad141c18720cca833ca8
Matvei Karibdzhanov <matveichrk2@gamil.com>
2024-08-26 13:50:57 +0900

    lost_data

--- sieve/all.sh
+++ sieve/all.sh
@@ -8,6 +8,8 @@ rm index_mc/*.index -f
 rm index_data/*.index -f
 rm -f fpda_pid.*
 
+> scripts/started.txt
+
 make
 
 for i in  07 09  11 13 15 17 19 21 23 25 27  31 33 35 37 39 41 43 45 47 49 51 53 55 61 63 65 67 69 71 73
