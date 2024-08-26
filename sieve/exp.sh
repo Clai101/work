90bbe8c4e5c0f6f42a2c5b43f6dffaaefa882b91
Matvei Karibdzhanov <matveichrk2@gamil.com>
2024-08-26 14:18:56 +0900

    lost_data

--- sieve/exp.sh
+++ sieve/exp.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-if test $exp
+if test $1
 then
-    exp=$exp
+    exp=$1
 else
     echo exp number is missing
     exit 1
 fi
 
+
 for ((i=0; i<30; i++)) do
 
 for ((j=0; j<10; j++)) do
-if [ `./check_statistics.sh  ${exp} $i $j` -ne "0" ] 
+if [ `./check_statistics.sh  $1 $i $j` -ne "0" ] 
 then 
-bsub -ql ./data.sh ${exp} ${i} ${j}
+bsub -ql ./data.sh ${1} ${i} ${j}
 fi
 done
 done
