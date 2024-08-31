7ee987e457c1daaafe328ece7c4637e70b8763cd
Matvei Karibdzhanov <matveichrk2@gamil.com>
2024-08-31 14:31:34 +0900

    Fast

--- sieve/reco.cc
+++ sieve/reco.cc
@@ -245,13 +245,13 @@ for(int j=0; j<X_c.size(); ++j){
     if((abs(ach.pType().lund()) == 423) or (abs(ach.pType().lund()) == 413)){
       t1->column("dsm", ach.p().m());
       t1->column("ch", abs(ach.pType().lund())-400);
-      t1->column("dsp", ach.p().mag());
+      t1->column("dsp", pStar(ach, elec, posi).vect().mag());
       t1->column("dm", ach.child(0).p().m());
-      t1->column("dp", ach.child(0).p().mag());
+      t1->column("dp", pStar(ach.child(0), elec, posi).vect().mag());
     }
     else{
       t1->column("dm", ach.p().m());
-      t1->column("dp", ach.p().vect());
+      t1->column("dp", pStar(ach, elec, posi).vect().mag());
     }
 
     t1->column("rm", (beam - (x_c.p())).m());    
