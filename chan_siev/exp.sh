e215b8635e328c52dfb73e76bf61e2ecfa8ecfa7
Matvei Karibdzhanov <matveichrk2@gamil.com>
2024-09-28 17:53:28 +0900

    err_chan_siev

---
+++ chan_siev/exp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+0;95;0c
+if test $1
+then
+    exp=$1
+else
+    echo exp number is missing
+    exit 1
+fi
+
+
+
+
+for ((i=0; i<30; i++)) do
+
+    for ((j=0; j<10; j++)) do
+            bsub -qs /sw/belle/local/bin/centos7-exec ./data.sh ${1} ${i} ${j}
+    done
+done
